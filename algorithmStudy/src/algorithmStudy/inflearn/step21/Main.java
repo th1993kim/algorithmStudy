79457e4973b3a5b5cf1c13ec9046d9eb5de6a65c
KIMTAEHYUN <th1993kim@gmail.com>
2022-06-29 03:09:40 +0900

    jango youtube

--- algorithmStudy/src/algorithmStudy/inflearn/step21/Main.java
+++ algorithmStudy/src/algorithmStudy/inflearn/step21/Main.java
@@ -49,4 +49,29 @@ public class Main {
 		return result;
 	}
 	
+	public int solution2(int n , int[][] arr) {
+		int answer = Integer.MIN_VALUE;
+		int sum1 , sum2;
+		for(int i=0; i<n; i++) {
+			sum1 = sum2 = 0;
+			for(int j=0; j<n; j++) {
+				sum1 += arr[i][j];
+				sum2 += arr[j][i];
+			}
+			
+			answer = Math.max(answer, sum1);
+			answer = Math.max(answer, sum2);
+		}
+		
+		sum1=sum2=0;
+		
+		for(int i=0; i<n; i++) {
+			sum1 += arr[i][i];
+			sum2 += arr[i][n-i-1];
+		}
+		answer = Math.max(answer, sum1);
+		answer = Math.max(answer, sum2);
+		return answer;
+	}
+	
 }
